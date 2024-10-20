49dec4c6378ef1d95bec9fe0acb474fd57e5a289
Carra7815 <carra7815@gmail.com>
2024-10-20 18:21:11 +0700

    fixed up tailwind file association

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
